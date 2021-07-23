9a35d420dbed0ef2a1d3c2cbeeb416e8e4812405
RogwiCajas <rogwinalex2@hotmail.com>
2021-07-23 16:44:10 -0500

    Case solved

--- src/main/java/IntegracionJenkins/RelationalOperator.java
+++ src/main/java/IntegracionJenkins/RelationalOperator.java
@@ -25,7 +25,7 @@ public class RelationalOperator {
     
     public void goodWidth(final int object_width, final int min_width, final int max_width){
         if (isGreater(object_width, min_width) && isLess(object_width, max_width)) {
-            System.out.println("The width is correct");
+            System.out.println("The width is CORRECT");
         } else {
             System.out.println("The width is INCORRECT");
         }
@@ -34,7 +34,7 @@ public class RelationalOperator {
     
     public void goodHeight(final int object_height, final int min_height, final int max_height){
         if (isGreater(object_height, min_height) && isLess(object_height, max_height)) {
-            System.out.println("The height is correct");
+            System.out.println("The height is CORRECT");
         } else {
             System.out.println("The height is INCORRECT");
         }
